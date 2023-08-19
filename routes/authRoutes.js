dc50e8342f739cd449b95b511ccc3b134b63cd4b
donalawa <awadonalcien12@gmail.com>
2023-08-19 23:12:59 +0100

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,29 +6,29 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 
-router.post('/signin', authController.signin);
+// router.post('/signin', authController.signin);
 
-router.post('/admin/signup', authController.adminSignup);
+// router.post('/admin/signup', authController.adminSignup);
 
-router.post('/admin/signin', authController.adminSignin);
+// router.post('/admin/signin', authController.adminSignin);
 
-router.get('/admin/profile', authMiddleware, authController.getAdminProfile);
+// router.get('/admin/profile', authMiddleware, authController.getAdminProfile);
 
-router.put('/profile-update', authMiddleware, authController.updateUserProfile);
+// router.put('/profile-update', authMiddleware, authController.updateUserProfile);
 
-router.put('/password-update', authMiddleware, authController.updateUserPassword);
+// router.put('/password-update', authMiddleware, authController.updateUserPassword);
 
-router.get('/user-earnings', authMiddleware, authController.getUserEarnings);
+// router.get('/user-earnings', authMiddleware, authController.getUserEarnings);
 
-router.get('/profile', authMiddleware, authController.getUserProfile);
+// router.get('/profile', authMiddleware, authController.getUserProfile);
 
-router.post('/user-withdraw', authMiddleware, authController.submitWithdrawalRequest);
+// router.post('/user-withdraw', authMiddleware, authController.submitWithdrawalRequest);
 
-router.post('/add-payment-info', authMiddleware, authController.addPaymentInfo);
+// router.post('/add-payment-info', authMiddleware, authController.addPaymentInfo);
 
-router.get('/get-all-users', authController.getAllUsers);
+// router.get('/get-all-users', authController.getAllUsers);
 
-router.put('/suspend-user/:userId', authController.suspendUser);
+// router.put('/suspend-user/:userId', authController.suspendUser);
 
 module.exports = router;
 
