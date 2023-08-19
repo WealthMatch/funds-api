389166adf6e7d551b0656c561da2c7cad214f283
donalawa <awadonalcien12@gmail.com>
2023-08-19 22:16:47 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// const authRoutes = require('./routes/authRoutes');
-// const investmentRoutes = require('./routes/investmentRoutes');
-// const packageRoutes = require('./routes/packageRoutes');
-// const siteDataRoutes = require('./routes/siteDataRoutes');
+const authRoutes = require('./routes/authRoutes');
+const investmentRoutes = require('./routes/investmentRoutes');
+const packageRoutes = require('./routes/packageRoutes');
+const siteDataRoutes = require('./routes/siteDataRoutes');
 const cors = require('cors');
 
 const app = express();
@@ -32,10 +32,10 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTo
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
-// app.use('/auth', authRoutes);
-// app.use('/investment', investmentRoutes);
-// app.use('/package', packageRoutes);
-// app.use('/site-data', siteDataRoutes);
+app.use('/auth', authRoutes);
+app.use('/investment', investmentRoutes);
+app.use('/package', packageRoutes);
+app.use('/site-data', siteDataRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
