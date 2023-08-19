2b131492cb9bc6ba190a9b93589890eb92d5adeb
donalawa <awadonalcien12@gmail.com>
2023-08-19 22:09:44 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // const authRoutes = require('./routes/authRoutes');
 // const investmentRoutes = require('./routes/investmentRoutes');
 // const packageRoutes = require('./routes/packageRoutes');
@@ -27,9 +27,9 @@ app.use("/status",(req,res) => {
 })
 
 // mongodb+srv://root:icui4cumise7@cluster0.3o7ko.mongodb.net/finance
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('Connected to MongoDB'))
-//   .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 // app.use('/auth', authRoutes);
