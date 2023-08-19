2958b42143a0193f534038284dc8419a6f7c5019
donalawa <awadonalcien12@gmail.com>
2023-08-19 22:51:11 +0100

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authController = require('../controllers/authControllers');
-const authMiddleware = require('../midlewares/authMidleware');
+// const authMiddleware = require('../midlewares/authMidleware');
 
 const router = express.Router();
 
@@ -12,19 +12,19 @@ router.post('/admin/signup', authController.adminSignup);
 
 router.post('/admin/signin', authController.adminSignin);
 
-router.get('/admin/profile', authMiddleware, authController.getAdminProfile);
+// router.get('/admin/profile', authMiddleware, authController.getAdminProfile);
 
-router.put('/profile-update', authMiddleware, authController.updateUserProfile);
+// router.put('/profile-update', authMiddleware, authController.updateUserProfile);
 
-router.put('/password-update', authMiddleware, authController.updateUserPassword);
+// router.put('/password-update', authMiddleware, authController.updateUserPassword);
 
-router.get('/user-earnings', authMiddleware, authController.getUserEarnings);
+// router.get('/user-earnings', authMiddleware, authController.getUserEarnings);
 
-router.get('/profile', authMiddleware, authController.getUserProfile);
+// router.get('/profile', authMiddleware, authController.getUserProfile);
 
-router.post('/user-withdraw', authMiddleware, authController.submitWithdrawalRequest);
+// router.post('/user-withdraw', authMiddleware, authController.submitWithdrawalRequest);
 
-router.post('/add-payment-info', authMiddleware, authController.addPaymentInfo);
+// router.post('/add-payment-info', authMiddleware, authController.addPaymentInfo);
 
 router.get('/get-all-users', authController.getAllUsers);
 
